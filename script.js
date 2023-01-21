349fd34f58d20229958014a39ac4b8cf4ed8f072
William <wiliancosta51@hotmail.com>
2023-01-21 19:59:48 -0300

    versão mandando e puxando informações

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let nome;
-
 inicializarProjeto();
+let mensagem = [];
+let caixaDeMensagens = document.querySelector(".mensagens");
 
 function inicializarProjeto() {
   const iniciar = document.querySelector(".container");
@@ -34,11 +34,33 @@ function login() {
   const participantes = axios.get(
     "https://mock-api.driven.com.br/api/v6/uol/participants"
   );
-
+  console.log(participantes);
   promise.catch(deuRuim);
 }
 function deuRuim(error) {
   if (error.response.status === 400) {
     login();
   }
+}
+buscarMensagens();
+function buscarMensagens() {
+  promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+  promise.then(mandarMensagem);
+}
+function mandarMensagem(mesage) {
+  caixaDeMensagens = "";
+  mensagem = mesage.data;
+
+  console.log(mensagem);
+
+  for (let i = 0; i < mensagem.length; i++) {}
+
+  caixaDeMensagens.innerHTML = `<div class="time">${mensagem.time}</div>
+
+  <div class="user">${mensagem.user}</div>
+
+  <div class="to">${mensagem.to}</div>
+
+  <div class="message">${mensagem.text}</div>`;
 }
